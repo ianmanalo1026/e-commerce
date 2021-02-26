b9a6d1822cbacb41f2af9ba3740e137539a7c6e6
Ian <ian.manalo1026@gmail.com>
2021-02-26 22:23:37 +0800

    update

--- ecomm/ecommpage/urls.py
+++ ecomm/ecommpage/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import ItemListView
+from .views import ItemListView, ItemDetailView
 
 
 urlpatterns = [
     path('', ItemListView.as_view(), name="store"),
-    # path('cart/', views.cart, name="cart"),
+    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     # path('checkout/', views.checkout, name="checkout"),
 ]
--- ecomm/ecommpage/views.py
+++ ecomm/ecommpage/views.py
@@ -1,18 +1,42 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import redirect, render ,get_object_or_404
+from django.utils import timezone
+from django.views.generic import ListView, DetailView
 from .models import Item, Order, OrderItem
 
 class ItemListView(ListView):
     model = Item
     template_name = 'ecommpage/store.html'
+    paginate_by = 10
     
 
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = "product.html"
+    
+def add_to_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_item, created = OrderItem.objects.get_or_create(
+        item=item,
+        user=request.user,
+        ordered=False
+    )
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+        else:
+            order.items.add(order_item)
 
-# def cart(request):
-#     context = {}
-#     return render(request, 'ecommpage/cart.html', context)
+    else:
+        ordered_date = timezone.now()
+        order = Order.objects.create(
+            user=request.user, ordered_date=ordered_date)
+        order.items.add(order_item)
 
-
+    return redirect("product", slug=slug)
 # def checkout(request):
 #     context = {}
 #     return render(request, 'ecommpage/checkout.html', context)
