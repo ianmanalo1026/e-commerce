daef7d15edbab7fe0967762886c100e78ae8f49f
Ian <ian.manalo1026@gmail.com>
2021-02-26 22:13:30 +0800

    updated the add_to_cart

--- ecomm/ecommpage/models.py
+++ ecomm/ecommpage/models.py
@@ -1,21 +1,52 @@
 from django.db import models
 from django.conf import settings
+from django.shortcuts import reverse
+from django.utils.text import slugify
+
+
+CATEGORY_CHOICES = (
+    ('C', 'Classic'),
+    ('CB', 'Comic Book'),
+    ('F', 'Fantacy'),
+    ('FC', 'Fiction'),
+    ('E', 'Educational'),
+    ('M', 'Motivational')
+)
 
 class Item(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     price = models.FloatField()
     stored = models.DateTimeField(auto_now_add=True)
-    quality = models.IntegerField()
+    item_quantity = models.IntegerField(default=1)
+    img = models.ImageField(upload_to='images/')
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    slug = models.SlugField(unique=True)
     
     def __str__(self) -> str:
         return self.title
-
+    
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.title)
+    #     super(Item, self).save(*args, **kwargs)
+    
+    def get_absolute_url(self):
+        return reverse("product", kwargs={"slug": self.slug})
+    
+    def get_add_to_cart_url(self):
+        return reverse("add_to_cart", kwargs={"slug": self.slug})
+        
 class OrderItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                            on_delete=models.CASCADE)
     item = models.OneToOneField(Item, on_delete=models.CASCADE)
-
+    quantity = models.IntegerField(default=1)
+    ordered = models.BooleanField(default=False)
+    
     def __str__(self):
-        return self.item.title
+        return f"{self.quantity} of {self.item.title}"
+    
+    
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
