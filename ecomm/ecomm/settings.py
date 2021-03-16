7f46c4d4a657f6b8c4c70fa8a6849ee4e5f36618
Ian <ian.manalo1026@gmail.com>
2021-03-16 22:10:09 +0800

    updateupdate

--- ecomm/ecomm/settings.py
+++ ecomm/ecomm/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+LOG
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
